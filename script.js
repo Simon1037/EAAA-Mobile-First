4b45a69e5d215b47dfbfdb53e6c7cf6b110ec78e
Simon1037 <nomis1037@gmail.com>
2024-04-19 01:53:13 +0200

    Navigation-bar working
    The responsive part is working, but the links still have to be worked on and the breakpoints- before finalizing.

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-/* This is used for changing the selector's property to either "none" or "flex" */
-function showSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "flex";
-}
-function hideSidebar() {
-  const sidebar = document.querySelector(".sidebar");
-  sidebar.style.display = "none";
-}
+const showSidebar = document.getElementById("showSidebar");
+const hideSidebar = document.getElementById("hideSidebar");
+const sidebar = document.querySelector(".sidebar");
+
+/* This moves the sidebar into view */
+
+showSidebar.addEventListener("click", () => {
+  sidebar.style.transform = "translateX(0)";
+});
+
+/* this will move the sidebar out of view */
+
+hideSidebar.addEventListener("click", () => {
+  sidebar.style.transform = "translateX(100%)";
+});
