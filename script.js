0947ba3eef65bd3aea78878b5562665e03364c2d
Simon1037 <nomis1037@gmail.com>
2024-04-18 13:39:20 +0200

    Started working on navigation (Not fully working)

---
+++ script.js
@@ -0,0 +1,9 @@
+/* This is used for changing the selector's property to either "none" or "flex" */
+function showSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "flex";
+}
+function hideSidebar() {
+  const sidebar = document.querySelector(".sidebar");
+  sidebar.style.display = "none";
+}
