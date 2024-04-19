a706d44cde177a4144b21866640cdd690b6981c1
Simon1037 <nomis1037@gmail.com>
2024-04-19 13:34:36 +0200

    Updated Index.html, CSS, JS
    Made header sticky.
    Made header hide or show when scrolling.

--- script.js
+++ script.js
@@ -13,3 +13,31 @@ showSidebar.addEventListener("click", () => {
 hideSidebar.addEventListener("click", () => {
   sidebar.style.transform = "translateX(100%)";
 });
+
+/* 
+This will hide the header when scrolling down or show when scrolling up
+create let variable to determine scroll position
+*/
+
+let lastScrollTop = 0;
+const navigation = document.querySelector("header");
+
+window.addEventListener("scroll", function () {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    navigation.style.top = "-65px"; // Adjust this value based on your header's height
+  } else {
+    // Scrolling up
+    navigation.style.top = "0";
+  }
+  lastScrollTop = scrollTop;
+});
+
+// Prevent default behavior for anchor tags with href="#"
+document.querySelectorAll('a[href="#"]').forEach((link) => {
+  link.addEventListener("click", function (event) {
+    event.preventDefault();
+  });
+});
